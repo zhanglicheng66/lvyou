fdbf00a73bf46a7b33e1adcd278b8f52d5b93bfb
张立诚 <zhanglicheng66666@163.com>
2019-07-22 22:41:42 +0800

    xiala better-scroll youcefudongabc

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,6 +2,8 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
@@ -11,15 +13,19 @@
 
     import CityHeader from './components/Header'
     import CitySearch from './components/Search'
+    import CityList from './components/List'
+    import CityAlphabet from './components/Alphabet'
     export default {
         name: "City",
         components:{
             CityHeader,
-            CitySearch
+            CitySearch,
+            CityList,
+            CityAlphabet
         },
         data (){
           return {
-            
+
           }
         },
         methods:{
---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,88 @@
+<template>
+  <ul class="list">
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+  </ul>
+</template>
+
+
+
+<script>
+    export default {
+        name: "CityAlphabet",
+        components:{
+
+        },
+        data (){
+          return {
+
+          }
+        },
+        methods:{
+
+        },
+        computed:{
+
+        },
+        watch: {
+
+        },
+         beforeCreate:function(){
+
+            },
+            created:function(){
+
+            },
+            beforeMount:function(){
+
+            },
+            mounted:function(){
+
+            },
+            beforeDestroy:function(){
+
+            },
+            destroyed:function(){
+
+            },
+            beforeUpdate:function(){
+
+            },
+            updated:function(){
+
+            },
+    }
+</script>
+
+
+
+
+
+<style lang="stylus" scoped>
+     @import '~@/assets/styles/varibles.styl'
+    @import '~@/assets/styles/mixins.styl'
+    .list
+        display :flex
+        flex-direction:column
+        justify-content :center
+        position :absolute
+        text-align:center
+        right:0
+        top:1.58rem
+        bottom:0
+        width:0.4rem
+        .item
+            line-height:0.4rem
+            text-align:center
+            color:$bgColor
+</style>
