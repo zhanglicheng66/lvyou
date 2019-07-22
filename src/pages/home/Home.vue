6f6c28328dab5aed9b0bd989cb45fbcc2eb3255f
张立诚 <zhanglicheng66666@163.com>
2019-07-22 14:34:10 +0800

    weekend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,6 +4,7 @@
     <home-swiper></home-swiper>
     <home-icons></home-icons>
     <home-recommend></home-recommend>
+    <home-weekend></home-weekend>
   </div>
 </template>
 
@@ -14,13 +15,15 @@
     import HomeSwiper from './components/Swiper'
     import HomeIcons from './components/Icons'
     import HomeRecommend from './components/Recommend'
+    import HomeWeekend from './components/weekend'
     export default {
         name: "Home",
         components:{
           HomeHeader,
           HomeSwiper,
           HomeIcons,
-          HomeRecommend
+          HomeRecommend,
+          HomeWeekend
         },
         data (){
           return {
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,115 @@
+<template>
+    <div>
+        <div class="title">周末去哪儿</div>
+        <ul>
+            <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+                <div class="item-img-wrapper">
+                    <img class="item-img"  :src='item.imgUrl'  alt="">
+                </div>
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+
+
+<script>
+    export default {
+        name: "HomeWeekend",
+        components:{
+
+        },
+        data (){
+          return {
+            recommendList:[{
+              id:'001',
+              imgUrl:'https://imgs.qunarzz.com/piao/fusion/1707/6a/ef538175bc41a802.jpg',
+              title:'日本樱花季节',
+              desc:'美丽的日本风情樱花飘落 大雪纷飞'
+            },{
+              id:'002',
+              imgUrl:'https://imgs.qunarzz.com/piao/fusion/1707/6a/ef538175bc41a802.jpg',
+              title:'日本樱花季节',
+              desc:'美丽的日本风情樱花飘落 大雪纷飞'
+            },{
+              id:'003',
+              imgUrl:'https://imgs.qunarzz.com/piao/fusion/1707/6a/ef538175bc41a802.jpg',
+              title:'日本樱花季节',
+              desc:'美丽的日本风情樱花飘落 大雪纷飞'
+            }]
+          }
+        },
+        methods:{
+
+        },
+        computed:{
+
+        },
+        watch: {
+
+        },
+         beforeCreate:function(){
+
+            },
+            created:function(){
+
+            },
+            beforeMount:function(){
+
+            },
+            mounted:function(){
+
+            },
+            beforeDestroy:function(){
+
+            },
+            destroyed:function(){
+
+            },
+            beforeUpdate:function(){
+
+            },
+            Updated:function(){
+
+            },
+    }
+</script>
+
+
+
+
+
+<style  lang='stylus'  scoped>
+    @import  '~@/assets/styles/mixins.styl'
+
+        .title
+            margin-top:0.2rem
+            line-height:0.8rem
+            background:#eee
+            text-indent:0.2rem
+        .item-img-wrapper
+            overflow :hidden
+            height:0
+            padding-bottom: 33.9%
+            .item-img
+                width:100%
+        .item-info
+            padding:0.1rem
+
+            .item-title
+                line-height:0.54rem
+                font-size:0.32rem
+                color:'black'
+                ellipsis()
+            .item-desc
+                line-height:0.4rem
+                ellipsis()
+
+
+
+</style>
